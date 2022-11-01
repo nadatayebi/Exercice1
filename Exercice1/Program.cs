a59e2ce4d0b7f24f574969363d9d16aab575577c
Acer <Acer@DESKTOP-PQOLN1N>
2022-11-01 19:28:06 +0100

    Add project files.

---
+++ Exercice1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("entrer un nombre :");
+            int p = Convert.ToInt32(Console.ReadLine());
+            bool point = true;
+            while (p <= 0)
+            {
+                Console.WriteLine("entrer un nombre positif :");
+                p = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 2; i < p; i++)
+            {
+                if ((p % i) == 0)
+                {
+                    point = false;
+                    break;
+                }
+            }
+            if (!point)
+            {
+                Console.WriteLine(" ce nombre n'est pas premier ");
+            }
+            else
+            {
+                Console.WriteLine(" ce nombre est premier ");
+            }
+        }
+    }
+}
